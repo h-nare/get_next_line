193407d567765bfd7c4933a8a1c6c728bcd8b28f
Nare Hakobyan <narhakob@c2r11s6.42yerevan.am>
2026-03-26 20:58:24 +0400

    esim

---
+++ get_next_line.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: narhakob <narhakob@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/26 17:21:17 by narhakob          #+#    #+#             */
+/*   Updated: 2026/03/26 20:53:29 by narhakob         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int find_n(char *a)
+{
+    int i = 0;
+    while(a[i])
+    {
+        if(a[i] == '\n')
+            return (1);
+        i++;
+    }
+    return (0);
+}
+char *get_next_line(int fd)
+{
+    int buffer;
+    static char *smth;
+    char a[BUFFER_SIZE +1];
+
+    buffer = read(fd,a,BUFFER_SIZE);
+
+    while((buffer > 0) && find_n)
+    {
+
+    }
+   
+    
+}
+
+int main()
+{
+    int fd;
+
+    fd = open("file.txt",O_RDONLY);
+    printf("%s",get_next_line(fd));
+    printf("%s",get_next_line(fd));
+    
+}
---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: narhakob <narhakob@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/26 17:18:41 by narhakob          #+#    #+#             */
+/*   Updated: 2026/03/26 20:18:33 by narhakob         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE 42
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#endif
